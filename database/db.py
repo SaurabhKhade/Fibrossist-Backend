d3792bc40ae53fd768f48fba308bf22de50aa73f
Saurabh Khade <khadesaurabh056@gmail.com>
2022-10-10 23:29:52 +0530

    Database access added

---
+++ database/db.py
@@ -0,0 +1,9 @@
+import os
+from pymongo import MongoClient
+import os
+
+cluster = MongoClient(
+        f'mongodb+srv://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASS")}@fibrossistcluster.1mfgosz.mongodb.net/?retryWrites=true&w=majority'
+    )
+
+db = cluster["Fibrossist"]
